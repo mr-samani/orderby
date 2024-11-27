27826a002846d1baa07956f682993cc7aab4b45c
mr.samani <mr.samani@outlook.com>
2024-11-27 10:58:09 +0330

    format

--- dist/order-by.d.ts
+++ dist/order-by.d.ts
@@ -5,4 +5,8 @@
  * @param ascending Whether to sort in ascending order. Default is true.
  * @returns A new array sorted by the given key/selector.
  */
-export declare function orderBy<T>(array: T[], selector: keyof T | ((item: T) => any), ascending?: boolean): T[];
+export declare function orderBy<T>(
+  array: T[],
+  selector: keyof T | ((item: T) => any),
+  ascending?: boolean
+): T[];
--- dist/order-by.js
+++ dist/order-by.js
@@ -1,5 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.orderBy = orderBy;
 /**
  * Extension method to sort an array by a given key or a custom selector function.
@@ -9,13 +9,11 @@ exports.orderBy = orderBy;
  * @returns A new array sorted by the given key/selector.
  */
 function orderBy(array, selector, ascending = true) {
-    return [...array].sort((a, b) => {
-        const aValue = typeof selector === 'function' ? selector(a) : a[selector];
-        const bValue = typeof selector === 'function' ? selector(b) : b[selector];
-        if (aValue < bValue)
-            return ascending ? -1 : 1;
-        if (aValue > bValue)
-            return ascending ? 1 : -1;
-        return 0;
-    });
+  return [...array].sort((a, b) => {
+    const aValue = typeof selector === 'function' ? selector(a) : a[selector];
+    const bValue = typeof selector === 'function' ? selector(b) : b[selector];
+    if (aValue < bValue) return ascending ? -1 : 1;
+    if (aValue > bValue) return ascending ? 1 : -1;
+    return 0;
+  });
 }
